b1c9cc531d06ca101f605305534fcc34cc4d3a4b
kariyawasam064 <155903350+kariyawasam064@users.noreply.github.com>
2026-03-09 14:52:31 +0530

    Remove hardcoded MongoDB URI from database.py
    Updated MongoDB connection string to be empty for security reasons.

--- backend-flastapi/Social/database.py
+++ backend-flastapi/Social/database.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 # Connect to MongoDB
-client = MongoClient("mongodb+srv://sajindu:saji1234@cluster0.bx77a.mongodb.net")  # replace with your URI if different
+client = MongoClient("")  # replace with your URI if different
 db = client["stress"]  # replace with your actual DB name
 
 # Define collection
